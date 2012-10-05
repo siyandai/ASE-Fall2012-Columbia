f0f90e3bad86660772b2730a267223ae7a1bbe4e
cyan <cyan.dai@gmail.com>
2012-10-04 20:15:59 -0400

    first commit after running "rails new water_supply"
    initial workspace

---
+++ water_supply/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaterSupply::Application.config.secret_token = '545cfb7758cd591daf1f1380a223d1c99a305fb09d862d12885142e41ed47af49df2b6013a81536057e181e964c40d12edcdf7c927d93e046ff3691cefbc279c'
